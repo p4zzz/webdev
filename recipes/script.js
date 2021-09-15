16fc5fea4e487831f0b23581341ed0e87b1e51cf
V1NR-4LF4KYN <caturix29@gmail.com>
2021-09-15 19:34:21 +0200

    work on button design. add random button. add footer.

--- recipes/script.js
+++ recipes/script.js
@@ -1,3 +1,6 @@
+const urls = ["list/lasagna.html", "list/pizza.html", "list/recipe.html"]
+
+
 // funcs for redirects to recipes
 function goToLasagna() {
 	window.location.href="list/lasagna.html";
@@ -5,4 +8,17 @@ function goToLasagna() {
 
 function goToPizza() {
 	window.location.href="list/pizza.html";
+}
+
+function goToRecipe() {
+	window.location.href="list/recipe.html"
+}
+
+
+
+
+// go to a random recipe
+function goToRandom() {
+	var randomLink = urls[Math.floor(Math.random()*urls.length)]
+	window.location.href=randomLink
 }
